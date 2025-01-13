392b0e4a8ed5d32fb3b5ce01fbf5d06fe105e4b7
Rishabh Giri <giririshabh98@gmail.com>
2025-01-13 10:49:15 +0530

    Create 13-01-2025.cpp

---
+++ 13-01-2025.cpp
@@ -0,0 +1,17 @@
+// Container With Most Water
+
+class Solution {
+  public:
+    int maxWater(vector<int> &arr) {
+        // code here
+        int ans=0, l=0, r=arr.size()-1;
+        
+        while(l<r){
+            ans=max(ans, min(arr[l],arr[r])*(r-l));
+            
+            if(arr[l]<arr[r]) l++;
+            else r--;
+        }
+        return ans;
+    }
+};
