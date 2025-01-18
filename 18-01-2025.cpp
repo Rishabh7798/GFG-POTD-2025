224105971d7a66d588c76762162bba26cdd5b124
Rishabh Giri <giririshabh98@gmail.com>
2025-01-18 12:13:23 +0530

    Create 18-01-2025.cpp

---
+++ 18-01-2025.cpp
@@ -0,0 +1,25 @@
+// Reverse a linked list
+
+/* Linked List Node structure:
+
+struct Node
+{
+    int data;
+    struct Node *next;
+}
+
+*/
+
+class Solution {
+  public:
+    Node* reverseList(struct Node* head) {
+        Node *curr=head, *prev=NULL, *next=curr->next;
+        while(curr){
+            next=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=next;
+        }
+        return prev;
+    }
+};
