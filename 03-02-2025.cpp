6896b57ccfeaba8529f783eeb0a01f2ff274ca2d
Rishabh Giri <giririshabh98@gmail.com>
2025-02-03 10:01:17 +0530

    Create 03-02-2025.cpp

---
+++ 03-02-2025.cpp
@@ -0,0 +1,25 @@
+// Height of Binary Tree
+
+/*
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+*/
+class Solution {
+  public:
+    // Function to find the height of a binary tree.
+    int height(Node* node) {
+        // code here
+        if (node == NULL)
+            return -1;
+        return 1 + max(height(node->left), height(node->right));
+    }
+};
