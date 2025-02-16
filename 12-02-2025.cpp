44e4b4d582dbdfccada05c4829e0f29f125ddab6
Rishabh Giri <giririshabh98@gmail.com>
2025-02-16 22:37:26 +0530

    Create 12-02-2025.cpp

---
+++ 12-02-2025.cpp
@@ -0,0 +1,35 @@
+k-th Smallest in BST
+
+/*Complete the function below
+
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+*/
+
+class Solution {
+  public:
+    void inorder(Node* root,int &k,int &ans){
+        if(!root)return ;
+        inorder(root->left,k,ans);
+        k--;
+        if(k==0){
+            ans=root->data;
+            return ;
+        }
+        inorder(root->right,k,ans);
+    }
+    int kthSmallest(Node *root, int k) {
+        int ans=-1;
+        inorder(root,k,ans);
+        return ans;
+      
+    }
+};
