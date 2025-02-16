fb9df4e0cc99b0d6d141f97b94d9059e4ea6ed2f
Rishabh Giri <giririshabh98@gmail.com>
2025-02-16 22:39:51 +0530

    Create 13-02-2025.cpp

---
+++ 13-02-2025.cpp
@@ -0,0 +1,35 @@
+Pair Sum in BST
+
+/*
+Node is as follows
+class Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+*/
+class Solution {
+  public:
+    void inorder(Node *root,vector<int>&ans){
+        if(!root) return;
+        inorder(root->left,ans);
+        ans.push_back(root->data);
+        inorder(root->right,ans);
+    }
+    bool findTarget(Node *root, int target) {
+        vector<int>ans;
+        inorder(root,ans);
+        int s=0,e=ans.size()-1;
+        while(s<e){
+            if(ans[s]+ans[e]==target) return true;
+            else if(ans[s]+ans[e]>target) e--;
+            else s++;
+        }
+        return false;
+    }
+};
