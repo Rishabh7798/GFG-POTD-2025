d923a730478720a25bc31f914b0d62733c9fdf4e
Rishabh Giri <giririshabh98@gmail.com>
2025-03-11 10:02:26 +0530

    Create 11-03-2025.cpp

---
+++ 11-03-2025.cpp
@@ -0,0 +1,20 @@
+Ways to Reach the n'th Stair
+
+class Solution {
+  public:
+    int countWays(int n) {
+        // your code here
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+        
+        int prev2 = 1, prev1 = 2, curr;
+        
+        for (int i = 3; i <= n; i++) {
+            curr = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        
+        return curr;
+    }
+};
