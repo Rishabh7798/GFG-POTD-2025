507627cc74c3aca9ce41d85f0cd8ff02f6dbdd29
Rishabh Giri <giririshabh98@gmail.com>
2025-03-24 00:55:00 +0530

    Create 24-03-2025.cpp

---
+++ 24-03-2025.cpp
@@ -0,0 +1,22 @@
+Matrix Chain Multiplication
+
+class Solution {
+  public:
+    int matrixMultiplication(vector<int> &arr) {
+        int n = arr.size();
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+        
+        // l is the chain length
+        for (int l = 2; l < n; l++) {
+            for (int i = 1; i < n - l + 1; i++) {
+                int j = i + l - 1;
+                dp[i][j] = INT_MAX;
+                for (int k = i; k < j; k++) {
+                    int cost = dp[i][k] + dp[k+1][j] + arr[i-1] * arr[k] * arr[j];
+                    dp[i][j] = min(dp[i][j], cost);
+                }
+            }
+        }
+        return dp[1][n-1];
+    }
+};
