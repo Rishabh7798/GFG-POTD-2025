936c613facb1c779d2be8c034d9392811a7f521c
Rishabh Giri <giririshabh98@gmail.com>
2025-04-18 00:34:02 +0530

    Create 18-04-2025.cpp

---
+++ 18-04-2025.cpp
@@ -0,0 +1,57 @@
+Implement Trie
+
+// User function template for C++
+class TrieNode {
+  public:
+    TrieNode* children[26];
+    bool isEnd;
+
+    TrieNode() {
+        isEnd = false;
+        for (int i = 0; i < 26; ++i)
+            children[i] = nullptr;
+    }
+};
+
+class Trie {
+  private:
+    TrieNode* root;
+
+  public:
+    Trie() {
+        root = new TrieNode();
+    }
+
+    void insert(string &word) {
+        TrieNode* node = root;
+        for (char ch : word) {
+            int index = ch - 'a';
+            if (node->children[index] == nullptr)
+                node->children[index] = new TrieNode();
+            node = node->children[index];
+        }
+        node->isEnd = true;
+    }
+
+    bool search(string &word) {
+        TrieNode* node = root;
+        for (char ch : word) {
+            int index = ch - 'a';
+            if (node->children[index] == nullptr)
+                return false;
+            node = node->children[index];
+        }
+        return node->isEnd;
+    }
+
+    bool isPrefix(string &word) {
+        TrieNode* node = root;
+        for (char ch : word) {
+            int index = ch - 'a';
+            if (node->children[index] == nullptr)
+                return false;
+            node = node->children[index];
+        }
+        return true;
+    }
+};
