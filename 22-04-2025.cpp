b6ddf25102da2c06568eb21742f370dcc8669434
Rishabh Giri <giririshabh98@gmail.com>
2025-04-22 00:44:37 +0530

    Create 22-04-2025.cpp

---
+++ 22-04-2025.cpp
@@ -0,0 +1,12 @@
+Unique Number I
+
+class Solution {
+  public:
+    int findUnique(vector<int> &arr) {
+        int result = 0;
+        for (int num : arr) {
+            result ^= num;
+        }
+        return result;
+    }
+};
